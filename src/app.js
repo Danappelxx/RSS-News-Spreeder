881f7c6a0e0841e955b45a46afe5a203bf8861f6
Dan Appel <Dan.appel00@gmail.com>
2015-04-30 20:44:59 -0700

    Add config webview
    Allows to create custom feeds from rss (or atom) feeds.

--- src/app.js
+++ src/app.js
@@ -2,45 +2,73 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 var ajax = require('ajax');
 var Vibe = require('ui/vibe');
+var Settings = require('settings');
 
 
+
+if(!Settings.option('menu_items')) {
+	Settings.option('menu_items', [
+		{
+			title: 'CNN',
+			url: 'http://rss.cnn.com/rss/cnn_topstories.rss'
+		},
+		{
+			title: 'BBC',
+			url: 'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml?edition=int'
+		},
+		{
+			title: 'NYT',
+			url: 'http://feeds.nytimes.com/nyt/rss/HomePage'
+		},
+		{
+			title: 'NPR',
+			url: 'http://www.npr.org/rss/rss.php?id=1001'
+		},
+		{
+			title: 'AP',
+			url: 'http://hosted.ap.org/lineups/USHEADS-rss_2.0.xml?SITE=SCAND&SECTION=HOME'
+		},
+		{
+			title: 'USA TODAY',
+			url: 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories'
+		},
+		{
+			title: 'WP',
+			url: 'http://feeds.washingtonpost.com/rss/world'
+		},
+		{
+			title: 'TOP NEWS',
+			url: 'http://feeds.reuters.com/reuters/topNews'
+		}
+	]);
+}
+
+//var settings_url = 'https://dvappel.me/stuff/spreeder-config.html#' + encodeURIComponent(JSON.stringify(Settings.option()));
+
+//console.log(settings_url);
+
+Settings.config(
+	{ url: 'https://dvappel.me/stuff/spreeder-config.html'},
+	function(e) {
+		console.log('opened config');
+		console.log(JSON.stringify(e));
+	},
+	function(e) {
+		console.log('closed config');
+		console.log(JSON.stringify(e.options));
+		
+		if( e.failed ){
+			console.log(e.response);
+		} else {
+			Settings.option('menu_items', e.options);
+		}
+		console.log(e);
+	}
+);
 
 var main = new UI.Menu({
 	sections: [{
-		items: [
-			{
-				title: 'CNN',
-				url: 'http://rss.cnn.com/rss/cnn_topstories.rss'
-			},
-			{
-				title: 'BBC',
-				url: 'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml?edition=int'
-			},
-			{
-				title: 'NYT',
-				url: 'http://feeds.nytimes.com/nyt/rss/HomePage'
-			},
-			{
-				title: 'NPR',
-				url: 'http://www.npr.org/rss/rss.php?id=1001'
-			},
-			{
-				title: 'AP',
-				url: 'http://hosted.ap.org/lineups/USHEADS-rss_2.0.xml?SITE=SCAND&SECTION=HOME'
-			},
-			{
-				title: 'USA TODAY',
-				url: 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories'
-			},
-			{
-				title: 'WP',
-				url: 'http://feeds.washingtonpost.com/rss/world'
-			},
-			{
-				title: 'TOP NEWS',
-				url: 'http://feeds.reuters.com/reuters/topNews'
-			}
-		]
+		items: Settings.option('menu_items')
 	}]
 });
 
@@ -64,21 +92,6 @@ main.on('select', function(e) {
 	};
 	request_json(rss_url, rss_callback);
 });
-
-function request_json(request_url, callback) {
-	ajax(
-	{
-		url: request_url,
-		type: 'json'
-	},
-	function(data, status, request) {
-		callback(data);
-	},
-	function(error, status, request) {
-		console.log(error);
-	}
-	);
-}
 
 function display_rss_feed(rss_feed) {
 	var menu_items = get_menu_items_from_rss_feed(rss_feed);
@@ -124,10 +137,6 @@ function display_rss_feed(rss_feed) {
 	});
 }
 
-function construct_readability_request_url(url) {
-	return 'http://access.alchemyapi.com/calls/url/URLGetText?apikey=2e290d8d3ec6fbb70b08debdbd67cca22f721e1f&outputMode=json&url=' + encodeURIComponent(url);
-}
-
 function get_menu_items_from_rss_feed(rss_feed) {
 	var menu_items = [rss_feed.length];
 	var i;
@@ -155,7 +164,6 @@ function read_content(text) {
 	wind.show();
 	
 	text = text.split(' ');
-
 	
 	var show_words = function () {
 		var show_word = function(next_index) {
@@ -179,4 +187,23 @@ function read_content(text) {
 		wait_then_show_word(0);
 	};
 	show_words();
+}
+
+function construct_readability_request_url(url) {
+	return 'http://access.alchemyapi.com/calls/url/URLGetText?apikey=2e290d8d3ec6fbb70b08debdbd67cca22f721e1f&outputMode=json&url=' + encodeURIComponent(url);
+}
+
+function request_json(request_url, callback) {
+	ajax(
+	{
+		url: request_url,
+		type: 'json'
+	},
+	function(data, status, request) {
+		callback(data);
+	},
+	function(error, status, request) {
+		console.log(error);
+	}
+	);
 }
